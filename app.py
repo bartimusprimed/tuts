433ef5584eca8f968bb28544c7324bfcb7269ef3
unknown <samuelbarthelemy@gmail.com>
2018-06-05 15:04:47 -0700

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from json import loads, dumps
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.debug = True
+socketio = SocketIO(app)
+
+
+@socketio.on('connected')
+def new_connection(data):
+    print("A new client has connected!")
+    emit('respond', {'data': 'Thanks for connecting'})
+
+
+@socketio.on('attention')
+def get_attention(data):
+    print("I WANT ATTENTION")
+
+
+if __name__ == '__main__':
+    socketio.run(app)
